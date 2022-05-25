08bb6787b27eef1437335d5519c0ee81700d5080
Noah-Bargisen <bargisenn@bzz.ch>
2022-05-25 11:45:26 +0200

    Working Backend

--- src/main/java/com/spring/web/service/UserDetailsImpl.java
+++ src/main/java/com/spring/web/service/UserDetailsImpl.java
@@ -7,8 +7,10 @@ import java.util.stream.Collectors;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import com.spring.web.model.UserModel;
 
 
